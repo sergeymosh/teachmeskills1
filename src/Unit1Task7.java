390f32675c36728777a3ea6fcfbc7016dec80fee
mosh <sergey.moshkin92@gmail.com>
2021-05-27 01:58:50 +0300

    Основные 7 задач ver1

--- src/Unit1Task7.java
+++ src/Unit1Task7.java
@@ -1,6 +1,6 @@
 public class Unit1Task7 {
     public static void main(String[] args) {
-        int a = 31;
+        int a = 1000;
         if(a == 2 || a == 22 || a == 32 || a == 42){
             System.out.println(a + " программиста");
         } else if(a == 1 || a == 21 || a == 31 || a == 41){
