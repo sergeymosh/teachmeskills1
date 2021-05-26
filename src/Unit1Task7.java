dcf6854ca33dfcab5f6f0d44b8110293e085dd5b
mosh <sergey.moshkin92@gmail.com>
2021-05-27 01:58:02 +0300

    Основные 7 задач

--- src/Unit1Task7.java
+++ src/Unit1Task7.java
@@ -5,7 +5,7 @@ public class Unit1Task7 {
             System.out.println(a + " программиста");
         } else if(a == 1 || a == 21 || a == 31 || a == 41){
             System.out.println(a + " программист");
-        } else if(a == 10 || a==100 || a==100){
+        } else if(a == 10 || a == 100 || a == 100 || a == 1000){
             System.out.println(a + " программистов");
         }
     }
