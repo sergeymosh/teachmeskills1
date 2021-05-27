4287467c23a2ffec13df4bda84f78441db549259
mosh <sergey.moshkin92@gmail.com>
2021-05-27 16:22:36 +0300

    Обновлена 8 задача, убрал лишний сканер1

--- src/Unit1Task8.java
+++ src/Unit1Task8.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Unit1Task8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите 2 числа:");
+        System.out.println("Введите 2 числа :");
 
 
         if (sc.hasNextInt()) {
