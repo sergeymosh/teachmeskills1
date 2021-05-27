e3ac02804a69ae47efe8e3c3db10cc74d6dfa325
sergeymosh <sergey.moshkin92@gmail.com>
2021-05-27 11:57:58 +0300

    Семь заданий готово, осталось всё оптимизировать

--- src/Unit1Task8.java
+++ src/Unit1Task8.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Unit1Task8 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число:");
 
+        int number1 = sc.nextInt();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите второе число:");
+
+        int number2 = sc.nextInt();
+        if(number1>number2){
+            System.out.println(number1);
+        } else {
+            System.out.println(number2);
+        }
     }
 }
