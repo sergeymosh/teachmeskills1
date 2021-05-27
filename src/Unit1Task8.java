c70716f3c7b4fa98ec299d2ca66a1aab7ec8fbcb
mosh <sergey.moshkin92@gmail.com>
2021-05-27 16:22:04 +0300

    Обновлена 8 задача, убрал лишний сканер

--- src/Unit1Task8.java
+++ src/Unit1Task8.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Unit1Task8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
         System.out.println("Введите 2 числа:");
 
 
-        if (sc.hasNextInt() && sc2.hasNextInt()) {
+        if (sc.hasNextInt()) {
             int number1 = sc.nextInt();
             System.out.println("Первое число " + number1);
-            int number2 = sc2.nextInt();
+            int number2 = sc.nextInt();
             System.out.println("Второе число " + number2);
             if(number1>number2){
                 System.out.println(number1 + " больше чем " + number2);
