bf76802d441969b6d2bb1b1499315e1efe665afa
mosh <sergey.moshkin92@gmail.com>
2021-05-28 18:49:27 +0300

    тестирование LowerCase1

---
+++ src/LowerCase1.java
@@ -0,0 +1,8 @@
+import java.util.Locale;
+
+public class LowerCase1 {
+    public static void main(String[] args) {
+        String name = "Sergey";
+        System.out.println(name.toUpperCase(Locale.ROOT));
+    }
+}
